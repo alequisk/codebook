e266b222062e2ce7d1da2115aa821e34a5a6671c
Alex Sousa <alexsousa1435@gmail.com>
2023-07-20 01:12:28 -0300

    refactor: change to namespace context

--- divide-and-conquer/closest-pair.cc
+++ divide-and-conquer/closest-pair.cc
@@ -1,59 +1,82 @@
-struct Point {
-  double x, y, id;
-  Point() : x(0), y(0), id(0) {}
-  Point(int x, int y, int id) : x(x), y(y), id(id) {}
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Before call the closest pair function, confirm that points are sorted by x */
+
+namespace closest_point {
+struct point {
+  int id;
+  double x, y;
+  friend ostream& operator<<(ostream& out, const point& p);
 };
 
-bool cmp_y(const Point& a, const Point& b) {
-  if (a.y == b.y) return a.x < b.x;
+bool point_compare_by_x(const point& a, const point& b) {
+  if (a.x == b.x)
+    return a.y < b.y;
+  return a.x < b.x;
+}
+bool point_compare_by_y(const point& a, const point& b) {
+  if (a.y == b.y)
+    return a.x < b.x;
   return a.y < b.y;
 }
 
-bool cmp_x(const Point& a, const Point& b) {
-  if (a.x == b.x) return a.y < b.y;
-  return a.x < b.x;
+/* Data */
+double min_pair_distance = 1e9;  // replace to max distance
+std::vector<point> points, strip;
+int a_id, b_id, sz;
+
+void init(int n) {
+  points.resize(n);
+  strip.resize(n);
+  sz = n;
 }
 
-int n;
-const int MAXN = 50005;
-Point points[MAXN], t[MAXN];
-double mindist = 1e12;
-int bx = 0, by = 1;
-
-void upd_ans(const Point& a, const Point& b) {
-  double d = sqrt(pow(a.x - b.x, 2) + pow(b.y - a.y, 2));
-  if (d < mindist) {
-    mindist = d;
-    bx = a.id;
-    by = b.id;
+void update_answer(const point& a, const point& b) {
+  double dist = sqrt(pow(a.x - b.x, 2) + pow(b.y - a.y, 2));
+  if (dist < min_pair_distance) {
+    a_id = a.id;
+    b_id = b.id;
+    min_pair_distance = dist;
   }
 }
-
-// range is x-sorted
-void closestPair(int l, int r) {
-  if (r - l <= 3) {
-    for (int i = l; i < r - 1; i++) {
-      for (int j = i + 1; j < r; j++) {
-        upd_ans(points[i], points[j]);
-      }
-    }
-    sort(points + l, points + r, cmp_y);
+/* [0, N - 1]- range */
+void closest_pair(int l, int r) {
+  if (r - l <= 3) {  // base case
+    for (int i = l; i < r; i++)
+      for (int j = i + 1; j <= r; j++)
+        update_answer(points[i], points[j]);
+    sort(points.begin() + l, points.begin() + r + 1);
     return;
   }
-  int m = (r + l) >> 1;
-  int midx = points[m].x;
+  int mid = (l + r) >> 1;
+  int mid_x = points[mid].x;
 
-  closestPair(l, m);
-  closestPair(m, r);
+  closest_pair(l, mid);
+  closest_pair(mid, r);
 
-  inplace_merge(points + l, points + m, points + r, cmp_y);
-
-  int tsz = 0;
-  for (int i = l; i < r; ++i) {
-    if (fabs(points[i].x - midx) < mindist) {
-      for (int j = tsz - 1, k = 0; j >= 0 && points[i].y - t[j].y < mindist && k < 6; --j, ++k)
-        upd_ans(points[i], t[j]);
-      t[tsz++] = points[i];
+  std::inplace_merge(points.begin() + l, points.begin() + mid, points.begin() + r + 1, point_compare_by_y);
+  int strip_size = 0;
+  for (int i = l; i <= r; i++) {
+    if (fabs(points[i].x - mid_x) < min_pair_distance) {
+      for (int j = strip_size - 1, k = 0; j >= 0 && points[i].y - strip[j].y < min_pair_distance && k < 6; --j, ++k) {
+        update_answer(points[i], strip[j]);
+      }
+      strip[strip_size++] = points[i];
     }
   }
 }
+
+double get_answer() {
+  return min_pair_distance;
+}
+
+pair<int, int> get_closest_points() {
+  return {a_id, b_id};
+}
+
+ostream& operator<<(ostream& out, const point& p) {
+  out << "(" << p.id << ", [" << p.x << "," << p.y << "])";
+  return out;
+}
+};  // namespace closest_point
